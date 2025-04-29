f0eeff55a846a023b924d1df1db00ad07c37110e
flamion <contact@flamion.dev>
2025-04-29 13:15:42 +0200

    Use Rust quicksort

--- ffm-java-side/src/main/java/software/shonk/seminar/hermes/RustFFMSorter.java
+++ ffm-java-side/src/main/java/software/shonk/seminar/hermes/RustFFMSorter.java
@@ -1,37 +1,17 @@
 package software.shonk.seminar.hermes;
 
+import com.sun.jdi.Value;
+
 import java.lang.foreign.*;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
 public class RustFFMSorter {
-    static {
-        Linker linker = Linker.nativeLinker();
-
-        SymbolLookup lib;
-        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-            lib = SymbolLookup.libraryLookup("../libquicksort/target/release/libquicksort.so", Arena.global());
-        } else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-            lib = SymbolLookup.libraryLookup("../libquicksort/target/release/libquicksort.dylib", Arena.global());
-        } else {
-            throw new UnsupportedOperationException("Unsupported OS");
-        }
-
-        quicksort = linker.downcallHandle(
-                lib.find("sum").orElseThrow(),
-                FunctionDescriptor.of(
-                        ValueLayout.JAVA_DOUBLE,
-                        ValueLayout.ADDRESS,
-                        ValueLayout.JAVA_LONG
-                )
-        );
-    }
-
     static MethodHandle quicksort;
 
     public static void main(String[] args) throws Throwable {
-        double[] values = {1.0, 2.0, 3.0, 4.0, 59.0};
+        double[] values = {5.0, 4.0, 4.1, 4.01, 4.001, 4.0001, 4.00001, 3.0, 2.0, 1.0};
 
         try (Arena arena = Arena.ofConfined()) {
             MemorySegment arraySegment = arena.allocate(ValueLayout.JAVA_DOUBLE, values.length);
@@ -39,26 +19,44 @@ public class RustFFMSorter {
                 arraySegment.setAtIndex(ValueLayout.JAVA_DOUBLE, i, values[i]);
             }
 
+            Linker linker = Linker.nativeLinker();
 
+            SymbolLookup lib;
+            if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+                lib = SymbolLookup.libraryLookup("../libquicksort/target/release/libquicksort.so", Arena.global());
+            } else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                lib = SymbolLookup.libraryLookup("../libquicksort/target/release/libquicksort.dylib", Arena.global());
+            } else {
+                throw new UnsupportedOperationException("Unsupported OS");
+            }
 
+            quicksort = linker.downcallHandle(
+                    lib.find("quicksort").orElseThrow(),
+                    FunctionDescriptor.ofVoid(
+                            ValueLayout.ADDRESS,
+                            ValueLayout.JAVA_LONG,
+                            ValueLayout.ADDRESS
+                    )
+            );
 
             // Define comparator function in Java
-/*            MemorySegment comparatorFn = linker.upcallStub(
+            MemorySegment comparatorFn = linker.upcallStub(
                     MethodHandles.lookup().findStatic(RustFFMSorter.class, "compareDoubles",
                             MethodType.methodType(int.class, double.class, double.class)),
                     FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.JAVA_DOUBLE, ValueLayout.JAVA_DOUBLE),
                     arena
-            );*/
+            );
 
-            //quicksort.invoke(arraySegment, (long) values.length, comparatorFn);
+
+            quicksort.invoke(arraySegment, (long) values.length, comparatorFn);
             // Karsten
-            double sum = (double) quicksort.invokeExact(arraySegment, (long) values.length);
-            System.out.println("Sum: " + sum);
+//            double sum = (double) quicksort.invokeExact(arraySegment, (long) values.length);
+//            System.out.println("Sum: " + sum);
 
             // Read back and print sorted values
-            /*for (int i = 0; i < values.length; i++) {
+            for (int i = 0; i < values.length; i++) {
                 System.out.print(arraySegment.getAtIndex(ValueLayout.JAVA_DOUBLE, i) + " ");
-            }*/
+            }
         }
     }
 
