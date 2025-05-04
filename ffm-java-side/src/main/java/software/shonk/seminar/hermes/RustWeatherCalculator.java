9620d79dae3b322deb24974a04f755b6f21e5714
nikkischnelle <nikki@schnelle.dev>
2025-05-04 13:34:25 +0200

    Rewrite RustWeatherCalculator

--- ffm-java-side/src/main/java/software/shonk/seminar/hermes/RustWeatherCalculator.java
+++ ffm-java-side/src/main/java/software/shonk/seminar/hermes/RustWeatherCalculator.java
@@ -3,36 +3,117 @@ package software.shonk.seminar.hermes;
 import java.lang.foreign.*;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.VarHandle;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RustWeatherCalculator {
-    static final GroupLayout DATA_POINT_LAYOUT = MemoryLayout.structLayout(
-            ValueLayout.JAVA_FLOAT.withName("temperature"),
-            ValueLayout.JAVA_FLOAT.withName("humidity"),
-            ValueLayout.JAVA_FLOAT.withName("wind_speed")
-    );
+    record DataPoint(float temperature, float humidity, float wind_speed) {
+        static GroupLayout MEMORY_LAYOUT = MemoryLayout.structLayout(
+                ValueLayout.JAVA_FLOAT.withName("temperature"),
+                ValueLayout.JAVA_FLOAT.withName("humidity"),
+                ValueLayout.JAVA_FLOAT.withName("wind_speed")
+        );
 
-    static final GroupLayout REGION_LAYOUT = MemoryLayout.structLayout(
-            ValueLayout.ADDRESS.withName("city_name"),
-            ValueLayout.ADDRESS.withName("data_points"),
-            ValueLayout.JAVA_LONG.withName("data_points_len")
-    );
+        static final VarHandle TEMPERATURE_HANDLE = MEMORY_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("temperature"));
+        static final VarHandle HUMIDITY_HANDLE = MEMORY_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("humidity"));
+        static final VarHandle WIND_SPEED_HANDLE = MEMORY_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("wind_speed"));
 
-    static final GroupLayout REGION_SEQUENCE_LAYOUT = MemoryLayout.structLayout(
-            ValueLayout.ADDRESS.withName("regions"),
-            ValueLayout.JAVA_LONG.withName("regions_len")
-    );
+        public void writeToSegment(MemorySegment segment, long offset) {
+            TEMPERATURE_HANDLE.set(segment, offset, this.temperature);
+            HUMIDITY_HANDLE.set(segment, offset, this.humidity);
+            WIND_SPEED_HANDLE.set(segment, offset, this.wind_speed);
+        }
+    }
+
+    record Region(String cityName, DataPoint[] dataPoints) {
+        static GroupLayout MEMORY_LAYOUT = MemoryLayout.structLayout(
+                ValueLayout.ADDRESS.withName("city_name"),
+                ValueLayout.ADDRESS.withName("data_points"),
+                ValueLayout.JAVA_LONG.withName("data_points_len")
+        );
+
+        static final VarHandle CITY_NAME_HANDLE = MEMORY_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("city_name"));
+        static final VarHandle DATA_POINTS_HANDLE = MEMORY_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("data_points"));
+        static final VarHandle DATA_POINTS_LEN_HANDLE = MEMORY_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("data_points_len"));
+
+        private MemorySegment allocateDatapoints(Arena arena) {
+            MemorySegment segment = arena.allocate(MemoryLayout.sequenceLayout(
+                    this.dataPoints.length,
+                    DataPoint.MEMORY_LAYOUT
+            ));
+
+            for (int i = 0; i < this.dataPoints.length; i++) {
+                MemorySegment datapointSegment = segment.asSlice(
+                        i * DataPoint.MEMORY_LAYOUT.byteSize(),
+                        DataPoint.MEMORY_LAYOUT.byteSize()
+                );
+
+                this.dataPoints[i].writeToSegment(datapointSegment, 0);
+            }
+
+            return segment;
+        }
+
+        public void writeToSegment(MemorySegment segment, long offset, Arena arena) {
+            // fine to allocate here, because they are both pointers in this struct
+            MemorySegment nameSegment = arena.allocateFrom(this.cityName);
+            MemorySegment datapointSegment = allocateDatapoints(arena);
+
+            CITY_NAME_HANDLE.set(segment, 0L, nameSegment);
+            DATA_POINTS_HANDLE.set(segment, 0L, datapointSegment);
+            DATA_POINTS_LEN_HANDLE.set(segment, 0L, dataPoints.length);
+        }
+
+        public MemorySegment allocateAndWrite(Arena arena) {
+            MemorySegment segment = arena.allocate(MEMORY_LAYOUT);
+            this.writeToSegment(segment, 0L, arena);
+            return segment;
+        }
+    }
+
+    record RegionSequence(Region[] regions) {
+        static GroupLayout MEMORY_LAYOUT = MemoryLayout.structLayout(
+                ValueLayout.ADDRESS.withName("regions"),
+                ValueLayout.JAVA_LONG.withName("regions_len")
+        );
+
+        static final VarHandle REGIONS_HANDLE = MEMORY_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("regions"));
+        static final VarHandle REGIONS_LEN_HANDLE = MEMORY_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("regions_len"));
+
+        private MemorySegment allocateRegions(Arena arena) {
+            MemoryLayout regionsSequenceLayout = MemoryLayout.sequenceLayout(
+                    this.regions.length,
+                    Region.MEMORY_LAYOUT
+            );
+
+            MemorySegment arraySegment = arena.allocate(regionsSequenceLayout);
+
+            for (int i = 0; i < this.regions.length; i++) {
+                MemorySegment regionSegment = this.regions[i].allocateAndWrite(arena);
+                arraySegment.asSlice(
+                        i * Region.MEMORY_LAYOUT.byteSize(),
+                        Region.MEMORY_LAYOUT.byteSize()
+                ).copyFrom(regionSegment);
+            }
+
+            return arraySegment;
+        }
+
+        public void writeToSegment(MemorySegment segment, long offset, Arena arena) {
+            REGIONS_HANDLE.set(segment, offset, this.allocateRegions(arena));
+            REGIONS_LEN_HANDLE.set(segment, offset, this.regions.length);
+        }
+
+        public MemorySegment writeToMemory(Arena arena) {
+            MemorySegment segment = arena.allocate(MEMORY_LAYOUT);
+            this.writeToSegment(segment, 0L, arena);
+            return segment;
+        }
+    }
 
     public static void main(String[] args) throws Throwable {
         Linker linker = Linker.nativeLinker();
-        SymbolLookup lib;
-        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-            lib = SymbolLookup.libraryLookup("../libquicksort/target/release/libffmexample.so", Arena.global());
-        } else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-            lib = SymbolLookup.libraryLookup("../libquicksort/target/release/libffmexample.dylib", Arena.global());
-        } else {
-            throw new UnsupportedOperationException("Unsupported OS");
-        }
+        SymbolLookup lib = lookupLibrary("../libquicksort/target/release/libffmexample");
 
         MethodHandle findWarmestRegion = linker.downcallHandle(
                 lib.find("find_warmest_region").orElseThrow(),
@@ -41,65 +122,63 @@ public class RustWeatherCalculator {
 
         try (Arena arena = Arena.ofConfined()) {
             String[] cities = {"Braunschweig", "Hannover", "Hamburg"};
-
             int pointsPerCity = 5;
 
-            MemoryLayout regionArrayLayout = MemoryLayout.sequenceLayout(cities.length, REGION_LAYOUT);
-            MemorySegment regionArray = arena.allocate(regionArrayLayout);
-            for (int i = 0; i < cities.length; i++) {
-                MemorySegment dataPoints = generateRandomDataPoints(arena, pointsPerCity);
-                MemorySegment cityName = arena.allocateFrom(cities[i]);
-                MemorySegment region = arena.allocate(REGION_LAYOUT);
+            MemorySegment regionArray = createRegions(arena);
 
-                VarHandle cityNameHandle = REGION_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("city_name"));
-                VarHandle dataPointsHandle = REGION_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("data_points"));
-                VarHandle dataPointsLenHandle = REGION_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("data_points_len"));
+            // Call Rust
+            MemorySegment warmestRegion = (MemorySegment) findWarmestRegion.invoke(regionArray);
 
-                cityNameHandle.set(region, 0L, cityName);
-                dataPointsHandle.set(region, 0L, dataPoints);
-                dataPointsLenHandle.set(region, 0L, pointsPerCity);
-
-                regionArray.asSlice(i * REGION_LAYOUT.byteSize()).copyFrom(region);
-            }
-
-            // Write the regions to a region sequence
-            MemorySegment regionSequence = arena.allocate(REGION_SEQUENCE_LAYOUT);
-            VarHandle regionsHandle = REGION_SEQUENCE_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("regions"));
-            VarHandle regionsLenHandle = REGION_SEQUENCE_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("regions_len"));
-            regionsHandle.set(regionSequence, 0L, regionArray);
-            regionsLenHandle.set(regionSequence, 0L, cities.length);
-
-            // Call the Rust function
-            MemorySegment warmestRegion = (MemorySegment) findWarmestRegion.invoke(regionSequence);
-
-            // Read the result, for that we have to reinterpret the segment as a REGION_LAYOUT
-            MemorySegment warmestRegionReinterpreted = warmestRegion.reinterpret(REGION_LAYOUT.byteSize());
-            VarHandle cityNameHandle = REGION_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("city_name"));
-
-            // print the city name
-            MemorySegment cityNameSegment = (MemorySegment) cityNameHandle.get(warmestRegionReinterpreted, 0L);
-            String cityName = cityNameSegment.reinterpret(Long.MAX_VALUE).getString(0);
-            System.out.println("Warmest region: " + cityName);
+            // Read and print result
+            printWarmestRegion(warmestRegion);
         }
     }
 
-    static MemorySegment generateRandomDataPoints(Arena arena, int count) {
-        MemorySegment dpArray = arena.allocate(DATA_POINT_LAYOUT.byteSize() * count);
+    private static MemorySegment createRegions(Arena arena) {
+        Region oldenburg = new Region("Oldenburg", new DataPoint[]{
+                new DataPoint(50, 10, 10),
+                new DataPoint(10, 10, 10),
+                new DataPoint(10, 10, 10),
+        });
 
-        VarHandle tempHandle = DATA_POINT_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("temperature"));
-        VarHandle humHandle = DATA_POINT_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("humidity"));
-        VarHandle windHandle = DATA_POINT_LAYOUT.varHandle(MemoryLayout.PathElement.groupElement("wind_speed"));
+        Region hannover = new Region("Hannover", new DataPoint[]{
+                new DataPoint(20, 10, 10),
+                new DataPoint(10, 10, 10),
+                new DataPoint(10, 10, 10),
+        });
 
-        for (int i = 0; i < count; i++) {
-            MemorySegment dp = dpArray.asSlice(i * DATA_POINT_LAYOUT.byteSize());
-            float temp = (float) ThreadLocalRandom.current().nextDouble(-20.0, 45.0);
-            float humidity = (float) ThreadLocalRandom.current().nextDouble(20.0, 90.0);
-            float wind = (float) ThreadLocalRandom.current().nextDouble(0.0, 10.0);
+        Region braunschweig = new Region("Braunschweig", new DataPoint[]{
+                new DataPoint(30, 10, 10),
+                new DataPoint(10, 10, 10),
+                new DataPoint(10, 10, 10),
+        });
 
-            tempHandle.set(dp, 0L, temp);
-            humHandle.set(dp, 0L, humidity);
-            windHandle.set(dp, 0L, wind);
+        RegionSequence regions = new RegionSequence(
+                new Region[] {
+                        oldenburg,
+                        hannover,
+                        braunschweig,
+                }
+        );
+
+        return regions.writeToMemory(arena);
+    }
+
+    private static void printWarmestRegion(MemorySegment regionSegment) {
+        MemorySegment warmestRegion = regionSegment.reinterpret(Region.MEMORY_LAYOUT.byteSize());
+        MemorySegment cityNameSegment = (MemorySegment) Region.CITY_NAME_HANDLE.get(warmestRegion, 0L);
+        String cityName = cityNameSegment.reinterpret(Long.MAX_VALUE).getString(0);
+        System.out.println("Warmest region: " + cityName);
+    }
+
+    private static SymbolLookup lookupLibrary(String basePath) {
+        String os = System.getProperty("os.name").toLowerCase();
+        String suffix = os.contains("mac") ? ".dylib" : os.contains("linux") ? ".so" : null;
+
+        if (suffix == null) {
+            throw new UnsupportedOperationException("Unsupported OS");
         }
-        return dpArray;
+
+        return SymbolLookup.libraryLookup(basePath + suffix, Arena.global());
     }
 }
